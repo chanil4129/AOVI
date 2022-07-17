653ead44716b9a3adc3a7f54deacda6f3a50aef6
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-07-18 00:37:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/vis_to_auralization/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vis_to_auralization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
